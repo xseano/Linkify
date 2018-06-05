8bcaed7385336a523b3e0890361e126924d13dbf
Sean Oberoi <xseano@yandex.com>
2018-06-05 10:45:31 -0700

    db/migration: create_links table to store urls and their respective hashes
    modifications to both create_users and create_password_resets migrations

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,7 +14,7 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index(255);
+            $table->string('email');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
--- database/migrations/2018_06_05_173905_create_links.php
+++ database/migrations/2018_06_05_173905_create_links.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateLinks extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('links', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('url');
+            $table->string('hash');
+            $table->integer('uid')->unsigned()->index('uid');
+            $table->timestamp('date')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
     /**
@@ -29,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('links');
     }
 }
