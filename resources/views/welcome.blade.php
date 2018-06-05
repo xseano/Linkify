c91853b51b4e8756c68532b81fc3f108456ba091
Sean Oberoi <xseano@yandex.com>
2018-06-05 13:52:04 -0700

    view/welcome: retrofit to project name and links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,15 +28,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    &#128279; Linkify &#128279;
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/xseano/">Author</a>
+                    <a href="https://github.com/xseano/Linkify">Source Code</a>
+                    <a href="{{ url('/about') }}">About</a>
                 </div>
             </div>
         </div>
