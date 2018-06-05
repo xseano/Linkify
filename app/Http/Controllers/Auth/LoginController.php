a39f0a4a4417bcddcb904f5ad7956f2e7b8d6abc
Sean Oberoi <xseano@yandex.com>
2018-06-05 11:32:03 -0700

    controller/login: rewrite architecture and logic

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,39 +1,56 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
     /**
-     * Where to redirect users after login.
+     * Handle an authentication attempt.
      *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
+     * @param  \Illuminate\Http\Request $request
      *
-     * @return void
+     * @return Response
      */
-    public function __construct()
+    public function login(Request $request)
     {
-        $this->middleware('guest')->except('logout');
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+
+            // user exists, perform checks
+            $user = Auth::user();
+
+            if ($user)
+            {
+                return redirect()->intended('home');
+            }
+        }
+        else
+        {
+            // user doesn't exist
+            $response = 'The credentials you provided did not match any accounts.';
+            return redirect()->intended('login')->withErrors(['unmet_requisites' => trans($response)]);
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->intended('login');
+    }
+
+    public function showLoginForm()
+    {
+        if (Auth::check())
+        {
+            // user is already authenticated
+            return redirect()->intended('home');
+        }
+        else
+        {
+            return view('auth.login');
+        }
     }
 }
