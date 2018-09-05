ccd2eeb7ec783e440085f7ee243cb56c47dfd2cd
Sean Oberoi <xsean.o@gmail.com>
2018-09-04 18:07:08 -0700

    general: include integers 0-9 as part of alphabet mix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->tokenizer = new URLToken('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        $this->tokenizer = new URLToken('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890');
     }
 
     /**
